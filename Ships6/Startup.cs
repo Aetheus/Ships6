19188bffcf1e00886f1ccf1d3612f4080622fcde
Aetheus <adrianhyk@gmail.com>
2015-07-02 04:50:47 +0800

    Initial PC commit

---
+++ Ships6/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Ships6.Startup))]
+namespace Ships6
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
