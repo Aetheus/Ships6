82757f71adadb1d23bf923e08d7fb8405ebba7c8
Aetheus <adrianhyk@gmail.com>
2015-07-20 14:11:20 +0800

    reservation changes

--- Ships6/Controllers/ReservationMakerController.cs
+++ Ships6/Controllers/ReservationMakerController.cs
@@ -1,6 +1,7 @@
 ﻿using Ships6.Models;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -17,10 +18,26 @@ namespace Ships6.Controllers
             return Content("invalid page");
         }
 
-
+        
         public ActionResult Make(int id)
         {
-            return Content("Placeholder");
+            var cabins = (from cb in db.Cabins
+                          where cb.CruiseID == id && !cb.CabinIsOccupied 
+                          select cb).ToList();
+
+            Cruise cruise = (   from cr in db.Cruises
+                                where cr.CruiseID == id
+                                select cr   ).First();
+
+            var cabinChoices = cabins.GroupBy(test => test.CabinType).Select(grp => grp.First());
+
+
+            ViewBag.cruise = cruise;
+            ViewBag.CabinChoices = cabinChoices;
+
+
+            return View();
+            //return Content("Placeholder");
         }
     }
 }
